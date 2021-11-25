e8024153f08a17feee99528ca7222c98b0b69c31
Harrison Cazzin <hcazzin@bayviewglen.ca>
2021-11-25 16:31:19 -0500

    Beginning code on boons, added flavour text (Harry)

---
+++ Zorkgreus/Boon.java
@@ -0,0 +1,41 @@
+package Zorkgreus;
+
+import java.util.ArrayList;
+
+public class Boon {
+    private String god;
+    private String boonName;
+    private ArrayList<String> stats = new ArrayList<>();
+
+    public Boon(){
+        god = "Unknown";
+        boonName = "Unknown";
+        stats = null;
+    }
+
+    public Boon(String god, String boonName){
+        this.god = god;
+        this.boonName = boonName;
+        this.stats = null;
+    }
+
+    public String displayBoon() {
+        return "Boon of " + god + " :" + boonName + "\n\n" + stats;
+    }
+
+    public String getGodName() {
+        return god;
+    }
+
+    public void setGod(String godName){
+        god = godName;
+    }
+
+    public String getBoonName(){
+        return boonName;
+    }
+
+    public void setBoonName(String boonName){
+        this.boonName = boonName;
+    }
+}
