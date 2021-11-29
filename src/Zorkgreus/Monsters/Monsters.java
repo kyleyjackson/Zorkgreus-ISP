2d78e3a97baad4001e9b3eddb8de58a42335c715
Fred He <fhe@bayviewglen.ca>
2021-11-29 14:27:59 -0500

    test

--- src/Zorkgreus/Monsters/Monsters.java
+++ src/Zorkgreus/Monsters/Monsters.java
@@ -1,22 +1,20 @@
 package src.Zorkgreus.Monsters;
 
 public class Monsters {
-    /*
-     * Monsters Superclass
-     * Each monster has its own priority
-     */
+    /* 
+    Monsters Superclass
+    Each monster has its own priority 
+    */
     private int atk;
     private int priority;
     private int def;
     private int hp;
-
-    public Monsters(int atk, int priority, int def, int hp) {
+    public Monsters(int atk, int priority, int def, int hp){
         this.atk = atk;
         this.priority = priority;
         this.def = def;
         this.hp = hp;
     }
-
-    public void displayMonsterMessage() {
+    public void displayMonsterMessage(){
     }
 }
