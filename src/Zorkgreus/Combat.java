ff9dd896efacce7ecc3e2c8fd12743e4735210fe
kyleyjackson <kjackson@bayviewglen.ca>
2021-11-30 14:59:34 -0500

    Moved Combat.java, was in a different folder for some reason

---
+++ src/Zorkgreus/Combat.java
@@ -0,0 +1,21 @@
+package Zorkgreus;
+
+public class Combat {
+    /**
+     * * Start combat - Determine who has higher combat priority - higher goes first
+     * *
+     * *
+     * *
+     * *
+     * *
+     * *
+     */
+
+    public void decreaseAtkPriority() {
+        
+    }
+
+    public void resetPriority() {
+
+    }
+}
